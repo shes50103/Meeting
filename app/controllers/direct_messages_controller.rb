6d98517f1f45a795a1eb7ae1061afa1a0a01b3e5
詹昇 <zhansheng@zhanshengdeMacBook-Air.local>
2017-05-16 14:30:12 +0800

    simplify

--- app/controllers/direct_messages_controller.rb
+++ app/controllers/direct_messages_controller.rb
@@ -1,10 +1,10 @@
-class DirectMessagesController < ApplicationController
-  before_action :authenticate_user!
+# class DirectMessagesController < ApplicationController
+#   before_action :authenticate_user!
 
-  def show
-    users = [current_user, User.find(params[:id])]
-    @chatroom = Chatroom.direct_message_for_users(users)
-    @chatroom_user = current_user.chatroom_users.find_by(chatroom_id: @chatroom.id)
-    render "chatrooms/show"
-  end
-end
+#   def show
+#     users = [current_user, User.find(params[:id])]
+#     @chatroom = Chatroom.direct_message_for_users(users)
+#     @chatroom_user = current_user.chatroom_users.find_by(chatroom_id: @chatroom.id)
+#     render "chatrooms/show"
+#   end
+# end
