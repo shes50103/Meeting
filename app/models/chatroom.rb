0795ed4f60c869238e4934726381545d3a6750be
詹昇 <zhansheng@zhanshengdeMacBook-Air.local>
2017-05-19 10:24:39 +0800

    element

--- app/models/chatroom.rb
+++ app/models/chatroom.rb
@@ -2,6 +2,7 @@ class Chatroom < ApplicationRecord
   has_many :chatroom_users, dependent: :destroy
   has_many :users, through: :chatroom_users
   has_many :messages, dependent: :destroy
+  has_many :elements, dependent: :destroy
   
   scope :public_channels, ->{ where(direct_message: false) }
   scope :direct_messages, ->{ where(direct_message: true) }
