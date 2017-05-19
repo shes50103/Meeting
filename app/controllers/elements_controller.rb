6404e03216a20a0f548d36a47c6dcdcb3295920a
詹昇 <zhansheng@zhanshengdeMacBook-Air.local>
2017-05-19 12:57:09 +0800

    element completion

--- app/controllers/elements_controller.rb
+++ app/controllers/elements_controller.rb
@@ -1,6 +1,8 @@
 class ElementsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_chatroom
+  before_action :set_element, only: [:edit, :update, :destroy]
+
 
   def new
     @element = @chatroom.elements.build
@@ -12,10 +14,30 @@ class ElementsController < ApplicationController
     render :new
   end
 
+  def edit
+  end
+
+  # def update
+  #   return redirect_to @chatroom if element.save
+  #   render :edit
+  # end
+
+  def update
+    return redirect_to @chatroom if @element.update_attributes(element_params)
+    render :edit
+  end
+
+  def destroy
+  end
+
   private
 
   def set_chatroom
     @chatroom = Chatroom.find(params[:chatroom_id])
+  end
+
+  def set_element
+    @element = @chatroom.elements.find(params[:id])
   end
 
   def element_params
